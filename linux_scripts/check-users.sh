f66acdefbba8cd52e04d4b9ea3f0e26df9103c32
johnmap007 <johnmap007@kali>
2025-10-28 16:37:12 -0400

    find out why check-user.sh is not working

--- linux_scripts/check-users.sh
+++ linux_scripts/check-users.sh
@@ -4,17 +4,24 @@ RED="\e[31m"
 GREEN="\e[32m"
 BLUE="\e[36m"
 RESET="\e[0m"
+BOLD="\e[1m"
 
 
-echo -e "$BLUE[i] Fetching list of valid users...$RESET"
-IFS= read -r -d '' -a USERS <<< "$(curl -sL https://raw.githubusercontent.com/LZX1000/IRSEC-2025/refs/heads/main/valid-linux-users.txt)"
+echo -e "$BLUE[i] Fetching list of valid users$RESET"
+IFS= read -r -d '' -a VALID_USERS <<< "$(curl -sL https://raw.githubusercontent.com/LZX1000/IRSEC-2025/refs/heads/main/valid-linux-users.txt)"
+
+# Store usernames, UIDs, and GIDs of every user in an array
+mapfile -t userinfo < <(cut -d : -f1,3,4 /etc/passwd)
 
 echo -e "$BLUE[i] Checking local users against list$RESET"
-while IFS= read -r user; do
-    if [[ ! " ${USERS[*]} " == *" $user "* ]]; then
-        echo -e "$RED[!] $user IS NOT A VALID USER!!!$RESET"
+
+usernames=("${userinfo[@]%%:*}")
+
+for user in "${usernames[@]}"; do
+    if [[ ! " ${VALID_USERS[*]} " == *" $user "* ]]; then
+        echo -e "$RED[!] $user is not a valid user$RESET"
     fi
-done < <(cut -d : -f1 /etc/passwd)
+done
 
 echo -e "$GREEN[*] Done!$RESET"
 
