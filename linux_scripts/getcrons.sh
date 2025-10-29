9da01f0e79a229ae980ef3749b1dfe4d66feb7ae
johnmap007 <66699730+johnmap007@users.noreply.github.com>
2025-10-29 10:48:20 -0400

    Update getcrons.sh

--- linux_scripts/getcrons.sh
+++ linux_scripts/getcrons.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 shells=()
-noninteractiveUsers=()
 while IFS= read -r line; do
 	[[ $line =~ ^# ]] && continue
 	shells+=("$line")
@@ -10,13 +9,9 @@ while IFS= read -r line; do
 	user=$(cut -d : -f1 <<< "$line")
 	shell=$(cut -d : -f7 <<< "$line")
 	if [[ $(basename "$shell") == "nologin" || $(basename "$shell") == "false" || ! " ${shells[*]} " =~ "$shell" ]]; then
-		noninteractiveUsers+=("$user")
 		continue
 	fi
 	echo "==================== Crontab for $user ===================="
 	sudo -u "$user" crontab -l
 done < /etc/passwd
-
-echo -e "\nThe following users have non-interactive shells and should be in the /etc/cron.deny list:"
-echo ${noninteractiveUsers[@]}
 
